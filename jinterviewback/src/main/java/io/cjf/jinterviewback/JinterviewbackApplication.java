3ac8e7e2dd263f03d223c3f7fc36a0fd1b96e49a
chenjiefei <nlpx_dc@hotmail.com>
2019-09-21 08:38:43 +0800

    first init

---
+++ jinterviewback/src/main/java/io/cjf/jinterviewback/JinterviewbackApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.jinterviewback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinterviewbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinterviewbackApplication.class, args);
+    }
+
+}
